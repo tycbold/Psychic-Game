243c5e97231eef92ec335ed61c121c8858191a1f
Tyler Seybold <tylerseybold@dhcp-10-102-177-98.wireless.northwestern.private>
2017-09-07 20:21:52 -0500

    game and css updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,58 @@
+// Variables
+
 var options = ["q", "w", "e", "r", "t", "y", "u", "i", "o", 
 "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", 
 "c", "v", "b", "n", "m"];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
-var guessedLetters = " ";
+var guessedLetters = [];
+var psychicGuess = options[Math.floor(Math.random()
+		*options.length)];
+
+// Functions
+
+function reset () {
+	guessesLeft = 10;
+	guessedLetters = [];
+	psychicGuess = options[Math.floor(Math.random()
+		*options.length)];
+}
 
 document.onkeyup = function () {
+
 	var userGuess = String.fromCharCode(event.keyCode)
 	.toLowerCase();
 
-	console.log (userGuess);
+	var pushedLetters = guessedLetters.indexOf(userGuess);
 
-
-
-	var psychicGuess = options[Math.floor(Math.random()
-		*options.length)];
-
-	console.log(psychicGuess);
-
+	
 	if (userGuess == psychicGuess) {
 		wins++;
-		alert("you win!");
+		reset ();
 	}
 
-	if (userGuess !== psychicGuess) {
+	if (userGuess !== psychicGuess && pushedLetters == -1) {
 		guessesLeft--;
+		guessedLetters.push(userGuess);
 	}
 
 	if (guessesLeft == 0) {
 		losses++;
-		alert("you lose!");
-		guessesLeft = 10;
+		reset ();
 	}
+	if (pushedLetters !== -1){
+		alert("You guessed this already!")
+
+	}
+
+// Pushing results to html file
 
 	var gameText = "<h1>Guess what letter I'm thinking of</h1>"
 	+ "<p>Wins: " + wins + "</p>"
 	+ "<p>Losses: " + losses + "</p>"
 	+ "<p>Guesses Left: " + guessesLeft + "</p>"
-	+ "<p>Guessed Letters: " + userGuess + ", </p>";
+	+ "<p>Guessed Letters: " + guessedLetters + ", </p>";
 
 	document.querySelector("#game").innerHTML = gameText;
 
